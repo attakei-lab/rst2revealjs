bc6aca6509dde1b70d68e202ced4c4f7e685e8ca
Kazuya Takei <myself@attakei.net>
2026-01-14 16:52:47 +0900

    feat: Output using project's template

--- src/rst2revealjs/writer.py
+++ src/rst2revealjs/writer.py
@@ -1,5 +1,8 @@
 """Writer for docutils."""
 
+from pathlib import Path
+
+from docutils import frontend
 from docutils.writers import html5_polyglot as base_writer
 
 
@@ -8,6 +11,17 @@ class RevealjsTranslator(base_writer.HTMLTranslator):
 
 
 class RevealjsWriter(base_writer.Writer):
+    default_template = Path(__file__).parent / "template.txt"
+
+    settings_spec = frontend.filter_settings_spec(
+        base_writer.Writer.settings_spec,
+        template=(
+            f'Template file. (UTF-8 encoded, default: "{default_template}")',
+            ["--template"],
+            {"default": default_template, "metavar": "<file>"},
+        ),
+    )
+
     def __init__(self):
         super().__init__()
         self.translator_class = RevealjsTranslator
