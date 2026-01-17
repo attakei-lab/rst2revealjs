cc0eba0e9705b9bcede4e583c9b3dbf6deef2b73
Kazuya Takei <myself@attakei.net>
2026-01-18 04:44:31 +0900

    feat(playground): Render compile source

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,9 +1,25 @@
 import { loadPyodide, version as pyodideVersion } from "pyodide";
 import Alpine from "alpinejs";
 
-window.Alpine = Alpine;
-window.pyodide = await loadPyodide({
+const pyodide = await loadPyodide({
   indexURL: `https://cdn.jsdelivr.net/pyodide/v${pyodideVersion}/full/`,
 });
+
+window.Alpine = Alpine;
+
+// Prepare docutils writer
+await pyodide.loadPackage("docutils");
+
+window.rst2revealjs = async (source: string): Promise<string> => {
+  await pyodide.runPython(`
+    from docutils.core import publish_string
+
+    def publish_revealjs(source):
+        output = publish_string(source, writer_name="html5").decode()
+        return output
+  `);
+  const publishRevealjs = pyodide.globals.get("publish_revealjs");
+  return publishRevealjs(source);
+};
 
 Alpine.start();
