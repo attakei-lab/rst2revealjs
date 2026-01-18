71c2058c710f1decdfa8c4706ef0d3b86b85fe7e
Kazuya Takei <myself@attakei.net>
2026-01-19 00:11:31 +0900

    refactor: Use html5 writer's template

--- src/rst2revealjs/writer.py
+++ src/rst2revealjs/writer.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from pathlib import Path
 
-from docutils import frontend, nodes
+from docutils import nodes
 from docutils.writers import html5_polyglot as base_writer
 
 from .engine import RevealjsEngine
@@ -63,15 +63,6 @@ class RevealjsTranslator(base_writer.HTMLTranslator):
 class RevealjsWriter(base_writer.Writer):
     default_template = Path(__file__).parent / "template.txt"
     default_revealjs_version = "5.2.1"
-
-    settings_spec = frontend.filter_settings_spec(
-        base_writer.Writer.settings_spec,
-        template=(
-            f'Template file. (UTF-8 encoded, default: "{default_template}")',
-            ["--template"],
-            {"default": default_template, "metavar": "<file>"},
-        ),
-    )
 
     def __init__(self):
         super().__init__()
