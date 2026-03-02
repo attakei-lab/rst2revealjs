bc4ed0fa56e61effeeee001f7ba711f20cb690ef
Kazuya Takei <myself@attakei.net>
2026-03-02 22:57:18 +0900

    feat(ide/vscode): Define command

--- ide-extensions/vscode/src/extension.ts
+++ ide-extensions/vscode/src/extension.ts
@@ -1,5 +1,19 @@
-import type * as vscode from "vscode";
+import * as vscode from "vscode";
 
-export function activate(_context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+  console.log(
+    'Congratulations, your extension "live-preview-for-rst2revealjs" is now active!',
+  );
+  const disposable = vscode.commands.registerCommand(
+    "live-preview-for-rst2revealjs.startPreview",
+    () => {
+      vscode.window.showInformationMessage(
+        "Display webview for live preview for rst2revealjs.",
+      );
+    },
+  );
+
+  context.subscriptions.push(disposable);
+}
 
 export function deactivate() {}
