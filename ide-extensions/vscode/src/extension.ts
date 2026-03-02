e058f015bf4d15098d1be40ad6a20734ba0021f7
Kazuya Takei <myself@attakei.net>
2026-03-03 00:48:54 +0900

    feat(ide/vscode): Implement simple preview

--- ide-extensions/vscode/src/extension.ts
+++ ide-extensions/vscode/src/extension.ts
@@ -1,4 +1,34 @@
+import * as cp from "node:child_process";
+import * as path from "node:path";
 import * as vscode from "vscode";
+
+type EditorContent = {
+  filename: string;
+  language: string;
+  body: string;
+};
+
+const panels: Map<string, vscode.WebviewPanel> = new Map();
+
+function getEditorContent(): EditorContent | null {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return null;
+  }
+  return {
+    filename: editor.document.fileName,
+    language: editor.document.languageId,
+    body: editor.document.getText(),
+  };
+}
+
+function updateContent(content: EditorContent) {
+  const panel = panels.get(content.filename);
+  if (panel) {
+    const proc = cp.spawnSync("uvx", ["rst2revealjs"], { input: content.body });
+    panel.webview.html = proc.stdout.toString();
+  }
+}
 
 export function activate(context: vscode.ExtensionContext) {
   console.log(
@@ -7,13 +37,42 @@ export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     "live-preview-for-rst2revealjs.startPreview",
     () => {
-      vscode.window.showInformationMessage(
-        "Display webview for live preview for rst2revealjs.",
-      );
+      const content = getEditorContent();
+      if (!content) {
+        vscode.window.showErrorMessage("No active editor found.");
+        return;
+      }
+      if (content.language !== "restructuredtext") {
+        vscode.window.showErrorMessage(
+          "The active file is not a reStructuredText file.",
+        );
+        return;
+      }
+      if (!panels.has(content.filename)) {
+        const webviewPanel = vscode.window.createWebviewPanel(
+          "livePreview",
+          `Live Preview: ${path.basename(content.filename)}`,
+          vscode.ViewColumn.Beside,
+          {
+            enableScripts: true,
+            retainContextWhenHidden: true,
+          },
+        );
+        panels.set(content.filename, webviewPanel);
+        updateContent(content);
+      }
     },
   );
 
   context.subscriptions.push(disposable);
+  {
+    const proc = cp.spawnSync("uv", ["--version"]);
+    if (proc.status !== 0) {
+      vscode.window.showWarningMessage(
+        "Currently, this extension requires uv.",
+      );
+    }
+  }
 }
 
 export function deactivate() {}
